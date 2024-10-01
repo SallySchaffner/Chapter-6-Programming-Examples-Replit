b9c9c27c98598e786a9c5f1aae7582c14f038535
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-10-01 20:31:10 +0000

    Added Falling Distance

--- .ccls-cache/@home@runner@Chapter-6-Programming-Examples-Replit/main.cpp
+++ .ccls-cache/@home@runner@Chapter-6-Programming-Examples-Replit/main.cpp
@@ -1,4 +1,35 @@
-//Chapter 6 Programming Examples, each program in its own branch
-#include <iostream>
+// Chapter 6 Programming Examples: Falling Distance
+// Create a table showing the distance traveled by an object for seconds 1 through 10.
 
-int main() { std::cout << "Chapter 6 Programming Examples\n"; }
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double getDistance(int);
+
+int main()
+{
+  double distance;
+
+  cout << fixed << setprecision(1) << showpoint;
+  cout << "Distance traveled in Meters" << endl;
+  cout << "Time (in seconds)     Distance (in meters)" << endl;
+
+  for (int seconds = 1; seconds <= 10; seconds++)
+  {
+    distance = getDistance(seconds);
+    cout << setw(5) << seconds << setw(25) << distance << endl;
+  }
+  cout << endl;
+
+  return 0;
+}
+
+double getDistance(int time)
+{
+  const double GRAVITY = 9.8;
+  double distance;
+  distance = .5 * GRAVITY * time * time;
+  return distance;
+}
